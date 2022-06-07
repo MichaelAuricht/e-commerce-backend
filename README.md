01a69114754f271377b325791f8d3c0fee2d869f
Michael Auricht <michaelauricht@live.com.au>
2022-06-07 20:08:48 +1000

    finish

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # e-commerce-backend
+
+## Description
+
+This is a REST API for an internet retail website using Express.js, Sequelize and MySQL.
+
+### Functionality
+
+This app is capable of creating, viewing and updating categories, products & tags.
+
+It can also delete entries from the database and establish associations between the different entities.
+
+### Installation & Usage Instructions
+
+Start with cloning this repo on your local machine:
+
+```sh
+$ git clone https://github.com/lukecp5/e-commerce-backend.git
+$ cd e-commerce-backend
+```
+
+To install and set up the application, run:
+```sh
+$ npm install employee-tracker
+```
+
+You will also need to place a .env file in the root directory of the project, in order to connect to your MySQL database. Here's an example:
+
+file: .env
+```
+DB_NAME=library_db
+DB_PASSWORD=
+DB_USER=root
+```
+
+To finish the set-up the application, complete the following steps:
+1. Create a MySQL database on your local machine using the *schema.sql* file located in the /db/ directory(From the MySQL CLI, source db/schema.sql)
+2. Seed the database with sample data to be used for testing purposes(Run *npm run seed* from inside the root directory of the project)
+
+Now you're ready to start the application! You can start the server by running: 
+```
+npm start
+```
+
+The server is running, now you can make requests to it through your desired method.
+
+---
+
+### Technologies Used
+
+Node, Sequelize, Express, MySQL
+
+## Screenshot
+
+## Video Link
+
+GitBash Input - https://drive.google.com/file/d/1yEEaCvQebNDg8-miz8kOabD-JTmCaZlh/view?usp=sharing
+
+Insomnia Display - 
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,60 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Category, Product, Tag } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try{
+    const categoryDataGet = await Category.findAll({include: [{model: Product}]}) 
+    res.status(200).json(categoryDataGet);
+  }catch(error){
+    res.status(500).json(error);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try{
+    const categoryDataGetId = await Category.findByPk(req.params.id, {include: [{ model: Product}]})
+    res.status(200).json(categoryDataGetId);
+  }catch(error)
+  {
+    res.status(500).json(error);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try{
+      const categoryDataPost = await Category.create(req.body)
+      res.status(200).json(categoryDataPost);  
+  }catch(error){
+    res.status(500).json(error)
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try{
+    const categoryDataPut = await Category.update(req.body, {where: { id: req.params.id}})
+    res.status(200).json(categoryDataPut);  
+  }catch(error){
+    res.status(500).json(error);
+  }
+  
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try{
+    const categoryDataDelete = await Category.destroy({where: { id: req.params.id}})
+    res.status(200).json(categoryDataDelete)
+  }catch(error){
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
